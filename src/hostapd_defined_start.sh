f9d863cd90213078c7c9680fa8cfcae232d9fe07
Gentleman-DE <70391383+Gentleman-DE@users.noreply.github.com>
2026-02-02 17:35:32 +0100

    fixed error

--- src/hostapd_defined_start.sh
+++ src/hostapd_defined_start.sh
@@ -1,10 +1,26 @@
+# SSID=$1
+# KEY=$2
+# sudo systemctl stop hostapd 
+# sudo cp /home/fap/src/hostapd.conf /etc/hostapd.conf
+# sudo echo "ssid="$SSID | sudo /usr/bin/tee --append /etc/hostapd.conf
+# echo "Trage passprase ein"
+# sudo echo "wpa_passphrase="$KEY |sudo /usr/bin/tee --append  /etc/hostapd.conf
+
+
+# sudo hostapd -B /etc/hostapd.conf -dd -t -f /tmp/hostapd.log
+
+#!/bin/bash
 SSID=$1
 KEY=$2
-sudo systemctl stop hostapd 
+
+sudo systemctl stop hostapd
+sudo killall hostapd 2>/dev/null
+
+sudo rfkill unblock wlan
+sudo ip link set wlan0 down
+sudo ip link set wlan0 up
+
 sudo cp /home/fap/src/hostapd.conf /etc/hostapd.conf
-sudo echo "ssid="$SSID | sudo /usr/bin/tee --append /etc/hostapd.conf
+echo "ssid=$SSID" | sudo tee -a /etc/hostapd.conf
 echo "Trage passprase ein"
-sudo echo "wpa_passphrase="$KEY |sudo /usr/bin/tee --append  /etc/hostapd.conf
-
-
-sudo hostapd -B /etc/hostapd.conf -dd -t -f /tmp/hostapd.log
+echo "wpa_passphrase=$KEY" | sudo tee -a /etc/hostapd.conf
