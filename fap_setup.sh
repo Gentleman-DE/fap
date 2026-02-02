fb55119f79e8ec047ee4ebf8ec9ee844d03e0f63
Gentleman-DE <70391383+Gentleman-DE@users.noreply.github.com>
2026-02-02 17:35:06 +0100

    Revert "fixed error + expert=1"
    This reverts commit 9d32ab26bb5bd2eb4d72335bf2f0ea0cf30374de.

--- fap_setup.sh
+++ fap_setup.sh
@@ -15,7 +15,7 @@
 # spiegelt aber keine Daten mehr auf eth1 und benötigt eth1 somit nicht mehr
 # EXPERT=1: OVS wird installiert und konfiguriert
 # EXPERT=0: OVS wird installiert, aber nicht konfiguriert 
-EXPERT=1
+EXPERT=0
 
 
 # Ab hier am besten nichts mehr ändern ;-)
